ec5fdac21e1a96516989d164d294e4b95638cf07
Brahyan A Vargas Rojas <ba.vargasro@gmail.com>
2023-02-10 21:53:40 -0500

    Cambio

--- scripts/Taller 1 BDML.R
+++ scripts/Taller 1 BDML.R
@@ -122,6 +122,10 @@ corrplot(res2$r, type="upper", order="hclust",
 
 ## Trasnformación de variables categoricas a dummy ##
 
+base2  <- base2 %>%
+  mutate(age2=age^2 , 
+         lnwage=log(ing_hr))
+
 #### Cambio de MaxEducLevel ####
 base2 %>%
   group_by(maxEducLevel) %>%
@@ -521,26 +525,6 @@ stargazer(reg4hr1, reg4hr2)
 # 5. Predicting earnings
 # ------------------------------------------------------------------------------------ #
 
-base2 <- base2 %>% 
-  mutate(maxprescolar=ifelse(maxEducLevel == 2, 1, 0))
-
-base2 <- base2 %>% 
-  mutate(maxprimariaincompleta=ifelse(maxEducLevel==3, 1, 0))
-
-base2 <- base2 %>% 
-  mutate(maxprimariacompleta=ifelse(maxEducLevel==4, 1, 0))
-
-base2 <- base2 %>% 
-  mutate(maxsecundariaincompleta=ifelse(maxEducLevel==5, 1, 0))
-
-base2 <- base2 %>% 
-  mutate(maxsecundariacompleta=ifelse(maxEducLevel==6, 1, 0))
-
-base2 <- base2 %>% 
-  mutate(maxterciaria=ifelse(maxEducLevel==7, 1, 0))
-
-base2 <- base2 %>% 
-  mutate(maxprescolar=ifelse(maxEducLevel == 2, 1, 0))
 ##a.
 set.seed(10101)
 #use 70% of the dataset as a training set and 30% as a test set. La base tiene variables que nos interesan
@@ -552,6 +536,28 @@ train  <- base2[sample, ]
 test   <- base2[!sample, ]
 
 ##b. 
+##b.
+
+estrato2+estrato3+estrato4+estrato5+estrato6
+empleadopublico+empleadodomestico+jornalero
+trabajadores2a5+trabajadores6a10+trabajadores11a50+mas50trabajadores
++maxprimariaincompleta+maxprimariacompleta+maxsecundariaincompleta+maxsecundariacompleta+maxterciaria
+
+## Modelo ln(w) en función de la edad y edad cuadrática ##
+regw_age2train <- lm(lnwage ~ age+ age2, data=train)
+stargazer(regw_age2train, type="text")
+
+test$regw_age2train<-predict(regw_age2train,newdata = test)
+
+with(test,mean((lnwage-reg4a_hrtrain)^2))
+
+## Modelo ln(w) en función del género ##
+reg4a_hrtrain <- lm(lnwage ~ female, data=train)
+stargazer(reg4a_hrtrain, type="text")
+
+test$reg4a_hrtrain<-predict(reg4a_hrtrain,newdata = test)
+
+with(test,mean((lnwage-reg4a_hrtrain)^2))
 
 ## Primer modelo ##
 model1<-lm(ing_hr~1,data=train)
@@ -578,8 +584,12 @@ test$model3<-predict(model3,newdata = test)
 with(test,mean((ing_hr-model3)^2))
 ## Cuarto modelo ##
 
-model4<-lm(ing_hr~totalHoursWorked+age+age^2+maxprimariaincompleta+maxprimariacompleta+maxsecundariaincompleta+maxsecundariacompleta+maxterciaria+
-             formal+sex+estrato1+fulltime+relab+sizeFirm,data=train)
+model4<-lm(ing_hr~totalHoursWorked+age+age^2+maxprimariaincompleta+maxprimariacompleta+maxsecundariaincompleta+
+             maxsecundariacompleta+maxterciaria+formal+sex+
+             estrato2+estrato3+estrato4+estrato5+estrato6+fulltime+
+             empleadopublico+empleadodomestico+jornalero+
+             trabajadores2a5+trabajadores6a10+trabajadores11a50+mas50trabajadores
+           ,data=train)
 test$model4<-predict(model4,newdata = test)
 
 with(test,mean((ing_hr-model4)^2))
@@ -589,8 +599,10 @@ stargazer(model4, type = "text")
 
 ## Quinto modelo ##
 
-model5<-lm(ing_hr~poly(age,2,raw=TRUE):poly(maxEducLevel,4,raw=TRUE):sex:formal:relab:sizeFirm+fulltime:totalHoursWorked+
-             estrato1+poly(sizeFirm,5,raw=TRUE):poly(totalHoursWorked,8,raw=TRUE)+maxprimariaincompleta:totalHoursWorked+maxprimariacompleta:totalHoursWorked+maxsecundariaincompleta:totalHoursWorked+
+model5<-lm(ing_hr~poly(age,2,raw=TRUE):sex:formal:relab:trabajadores2a5+trabajadores6a10
+           +trabajadores11a50+mas50trabajadores+fulltime:totalHoursWorked+
+             estrato1+poly(trabajadores2a5,5,raw=TRUE):poly(totalHoursWorked,8,raw=TRUE)+
+             maxprimariaincompleta:totalHoursWorked+maxprimariacompleta:totalHoursWorked+maxsecundariaincompleta:totalHoursWorked+
            maxsecundariacompleta:totalHoursWorked+maxterciaria:totalHoursWorked
            ,data=train)
 test$model5<-predict(model5,newdata = test)
